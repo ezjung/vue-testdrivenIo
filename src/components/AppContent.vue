9de9e3da41a20cfab8a212d98ee3d0dcbf9b51c0
Sungsoon Park <sungsoon.park@gmail.com>
2022-09-17 13:41:26 -0700

    2.19-start: Compoenet (Part3)

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,12 +1,34 @@
 <script setup>
-import { ref } from '@vue/reactivity'
-
-const message = ref('Content goes here!')
-</script>
+  import { ref } from '@vue/reactivity'
+  import ListUsers from './ListUsers.vue';
+  
+  const message = ref('List of Users:')
+  const users = ref([
+    {
+      id: 1,
+      name: 'User #1',
+      username: 'user_1',
+      email: 'email1@gmail.com',
+    },
+    {
+      id: 2,
+      name: 'User #2',
+      username: 'user_2',
+      email: 'email2@gmail.com',
+    },
+    {
+      id: 3,
+      name: 'User #3',
+      username: 'user_3',
+      email: 'email3@gmail.com',
+    },
+  ])
+  </script>
 
 <template>
     <main>
-        <h1>{{ message }}</h1>
+      <h1>{{ message }}</h1>
+      <ListUsers :users="users" />
     </main>
 </template>
 
---
+++ src/components/ListUsers.vue
@@ -0,0 +1,53 @@
+<script setup>
+defineProps({
+    message: {type: String, required: true},
+    users: {type: Array, required: true}
+})    
+</script>
+
+<template>
+    <table>
+        <!-- Table Header Row -->
+        <tr>
+            <th>User ID</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+        </tr>
+        <!-- Table Elements (Rows) -->
+        <tr v-for="user in users" v-bind:key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.name }}</td>
+            <td>{{ user.username }}</td>
+            <td>{{ user.email }}</td>
+        </tr>
+    </table>
+</template>
+
+<style scoped>
+table {
+  margin-top: 0.5em;
+  table-layout: fixed;
+  border-collapse: collapse;
+}
+
+td, th {
+  border: 1px solid #88BBD6;
+  padding: 0.8rem;
+  overflow: hidden;
+}
+
+th {
+  text-align: center;
+  background-color: #88BBD6;
+  color: black;
+}
+
+tr:nth-child(even) {
+  background-color: #CDCDCDB0;
+}
+
+tr:nth-child(odd) {
+  background-color: #CDCDCD40;
+}
+</style>
